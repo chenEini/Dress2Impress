ec509e87e3ba62d2564dc308dcc1e8f6163ed67c
Chen Eini <chen.eini000@gmail.com>
2020-08-09 01:09:34 +0300

    outfit list using fragments

--- app/src/main/java/com/chen/dress2impress/HomeFragmentActivity.java
+++ app/src/main/java/com/chen/dress2impress/HomeFragmentActivity.java
@@ -7,21 +7,31 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class HomeFragmentActivity extends AppCompatActivity {
+import com.chen.dress2impress.model.Outfit;
+
+public class HomeFragmentActivity extends AppCompatActivity implements OutfitsListFragment.Delegate {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_fragment);
 
-        OutfitsListFragment outfitsListFragment = (OutfitsListFragment) getSupportFragmentManager().findFragmentById(R.id.outfit_list_fragment);
-        outfitsListFragment.setTitle("Hello From Chen");
+        OutfitsListFragment outfitsListFragment = new OutfitsListFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.home_fragment_container, outfitsListFragment, "TAG");
+        transaction.commit();
+    }
 
+    void openOutfitDetails() {
         OutfitDetailsFragment outfitDetailsFragment = new OutfitDetailsFragment();
-
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.add(R.id.home_fragment_container, outfitDetailsFragment, "TAG");
         transaction.addToBackStack("TAG");
         transaction.commit();
+    }
+
+    @Override
+    public void onItemSelected(Outfit outfit) {
+        openOutfitDetails();
     }
 }
--- app/src/main/java/com/chen/dress2impress/OutfitsListFragment.java
+++ app/src/main/java/com/chen/dress2impress/OutfitsListFragment.java
@@ -1,13 +1,23 @@
 package com.chen.dress2impress;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.chen.dress2impress.model.Model;
+import com.chen.dress2impress.model.Outfit;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +25,13 @@ import android.widget.TextView;
  * create an instance of this fragment.
  */
 public class OutfitsListFragment extends Fragment {
-    private String title;
-    private TextView titleTextView;
+    RecyclerView outfitsList;
+    List<Outfit> outfitsData;
 
-    void setTitle(String title) {
-        if (titleTextView != null) {
-            titleTextView.setText(title);
-        }
-        this.title = title;
+    Delegate parent;
+
+    interface Delegate {
+        void onItemSelected(Outfit outfit);
     }
 
     // TODO: Rename parameter arguments, choose names that match
@@ -35,7 +44,7 @@ public class OutfitsListFragment extends Fragment {
     private String mParam2;
 
     public OutfitsListFragment() {
-        // Required empty public constructor
+        outfitsData = Model.instance.getAllOutfits();
     }
 
     /**
@@ -66,13 +75,104 @@ public class OutfitsListFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_outfits_list, container, false);
 
-        titleTextView = view.findViewById(R.id.outfit_list_fragment_title);
+        outfitsList = view.findViewById(R.id.outfit_list);
+        outfitsList.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        outfitsList.setLayoutManager(layoutManager);
+
+        OutfitsListAdapter adapter = new OutfitsListAdapter();
+        outfitsList.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onClick(int position) {
+                Log.d("TAG", "Row number" + position + "was clicked");
+                Outfit outfit = outfitsData.get(position);
+                parent.onItemSelected(outfit);
+            }
+        });
 
         return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof Delegate) {
+            parent = (Delegate) getActivity();
+        } else {
+            throw new RuntimeException(context.toString()
+                    + "outfit list parent activity must implement delegate");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        parent = null;
+    }
+
+    static class OutfitRowViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        TextView description;
+
+        public OutfitRowViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
+            super(itemView);
+            title = itemView.findViewById(R.id.outfit_row_title);
+            description = itemView.findViewById(R.id.outfit_row_description);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onClick(position);
+                        }
+                    }
+                }
+            });
+        }
+
+        public void bind(Outfit outfit) {
+            title.setText(outfit.title);
+            description.setText(outfit.description);
+        }
+    }
+
+    interface OnItemClickListener {
+        void onClick(int position);
+    }
+
+    class OutfitsListAdapter extends RecyclerView.Adapter<OutfitRowViewHolder> {
+        private OnItemClickListener listener;
+
+        void setOnItemClickListener(OnItemClickListener listener) {
+            this.listener = listener;
+        }
+
+        @NonNull
+        @Override
+        public OutfitRowViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.outfit_list_row, parent, false);
+            OutfitRowViewHolder viewHolder = new OutfitRowViewHolder(view, listener);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull OutfitRowViewHolder holder, int position) {
+            Outfit outfit = outfitsData.get(position);
+            holder.bind(outfit);
+        }
+
+        @Override
+        public int getItemCount() {
+            return outfitsData.size();
+        }
     }
 }
