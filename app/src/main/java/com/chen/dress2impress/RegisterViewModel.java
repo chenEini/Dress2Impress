a6df76f5080d7fb52999535120c7096192317153
chenEini <chen.eini000@gmail.com>
2020-08-12 21:39:14 +0300

    Handle user login

--- app/src/main/java/com/chen/dress2impress/RegisterViewModel.java
+++ app/src/main/java/com/chen/dress2impress/RegisterViewModel.java
@@ -2,6 +2,14 @@ package com.chen.dress2impress;
 
 import androidx.lifecycle.ViewModel;
 
+import com.chen.dress2impress.model.user.User;
+import com.chen.dress2impress.model.user.UserModel;
+
 public class RegisterViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+
+    public void register(String email, String password, String name, String imageUrl, UserModel.Listener listener) {
+        User user = new User(email, name, imageUrl);
+        UserModel.instance.register(user, password, listener);
+    }
 }
